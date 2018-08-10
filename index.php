52efd6772484a13b7d7f4a9b28a2f21c39738ed9
Elmas Sayed <elmassayed@gmail.com>
2018-08-10 14:32:12 -0700

    form

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php 
     // Message Vars
     $msg = "";
@@ -14,11 +13,19 @@
         // VALIDATION- Check required fields--elmas: chking the no empty fields
         if(!empty($name) && !empty($email) && !empty($message)){
             // validation passed
-            echo "all good";
+            // Check email ---elmas:after making sure there are no empty , we check if email is in correct fromat
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+                // Failed
+                $msg = 'Please use a valid email';
+                $msgClass = "alert-danger";               
+            } else {
+                // Passed
+                echo 'passed';
+            }
         }else{
             // validation Failed
             $msg = 'Please fill in all fields';
-            $msgClass = "alert-primary";
+            $msgClass = "alert-danger";
         } 
     }
 ?>
@@ -48,18 +55,29 @@
             <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                 <div class="form-group">
                     <label>Name</label>
-                    <input type = "text" name="name" class="form-control" value="">
+                    <input type = "text" name="name" class="form-control" value="<?php echo isset ($_POST['name']) ? $name : ''; ?>">
                 </div>
                 <div class="form-group">
                     <label>Email</label>
-                    <input type = "text" name="email" class="form-control" value="">
+                    <input type = "text" name="email" class="form-control" value="<?php echo isset ($_POST['email']) ? $email : ''; ?>">
                 </div>
                 <div class="form-group">
                     <label>Message</label>
-                    <textarea  name="message" class="form-control"></textarea>
+                    <textarea  name="message" class="form-control"><?php echo isset ($_POST['message']) ? $message : ''; ?></textarea>
                 </div>
                 <br>
                 <button type="submit" name="submit" class="btn btn-primary">Submit</button>
         </div>         
     </body>
 </html>
+
+<!-- 
+       Pseudocode:
+    1) check for empty fields.... 
+    2) put alert messages if not valid
+    3) check for email validity
+    4) put alert messages if not valid
+    5) incase of error in some fields, keep other fields filled.
+    6) give htmlspecialchars for inputs when setting their variables (these will detele any unwanted characs in the input field)
+
+ -->
